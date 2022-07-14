40f7e2e169ce2a9203d69b098875d720c92350fe
Matthew Arofin <Matt.arofin@gmail.com>
2022-07-14 15:54:08 -0400

    Initial setup commit

--- api/server.js
+++ api/server.js
@@ -3,7 +3,16 @@ const server = express();
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
+const Actions = require('./actions/actions-router');
 // Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+const Projects = require('./projects/projects-router');
 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    console.log()
+    res.send(`<h1>`)
+})
+
+// Do NOT `server.listen()` inside this file!
 module.exports = server;
